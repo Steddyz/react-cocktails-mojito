8c6fc1c38fded55bf6e5b9a1d239fd98fe3965b2
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-08-14 18:09:08 +0300

    finished contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 import { openingHours, socials } from "../../constants/const";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
 
 const Contact = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#contact h2", "words");
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+      },
+      ease: "power1.inOut",
+    });
+
+    timeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .from("#contact h3, #contact p", {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .to("#f-right-leaf", {
+        y: "-50",
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to(
+        "#f-left-leaf",
+        {
+          y: "-50",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+  });
+
   return (
     <footer id="contact">
       <img
